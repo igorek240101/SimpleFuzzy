aa938e48fca2a8176b6988b6e361091ad1234d59
Acrist671 <mishamironovmma@gmail.com>
2024-07-10 22:45:26 +0600

    SF-10

---
+++ SimpleFuzzy.ExampleModules/SimpleFuzzy.SimpleModule/IntSet.cs
@@ -0,0 +1,68 @@
+﻿using SimpleFuzzy.Abstract;
+
+class IntSet : IObjSet<int>
+{
+    private List<int> objects;
+    private int currentindex;
+
+    public IntSet()
+    {
+        objects = new List<int>();
+        for (int i = 0; i < 100; i++)
+        {
+            objects.Add(i+1);
+        }
+        currentindex = 0;
+    }
+
+    public int Extraction()
+    {
+        return objects[currentindex];
+    }
+
+    public void MoveNext()
+    {
+        if (IsEnd())
+        {
+            throw new InvalidOperationException("Текущий элемент последний. Переход к следующему невозможен");
+        }
+        else
+        {
+            currentindex++;
+        }
+    }
+
+    public void ToFirst()
+    {
+        currentindex = 0;
+    }
+
+    public bool IsEnd()
+    {
+        if (currentindex+1 == objects.Count)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class SeriesDemo
+{
+    static void Main()
+    {
+        IntSet intSet = new IntSet();
+
+        for (int i = 0;i < 100; i++)
+        {
+            Console.WriteLine(intSet.Extraction());
+            intSet.MoveNext();
+        }
+        Console.WriteLine(intSet.Extraction());
+    }
+}
+
+
---
+++ SimpleFuzzy/SimpleFuzzy.Abstract/IObjSet.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleFuzzy.Abstract
+{
+    public interface IObjSet<T>
+    {
+        T Extraction();
+        void MoveNext();
+        void ToFirst();
+        bool IsEnd();
+    }
+}
