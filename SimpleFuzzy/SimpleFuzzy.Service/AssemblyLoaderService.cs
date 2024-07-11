982e3859bb8c7d93dd9d6e064ce97242b7271e4a
damirbik <dbikchurayev@bk.ru>
2024-07-11 19:12:18 +0300

    SF-9

---
+++ SimpleFuzzy/SimpleFuzzy.Service/AssemblyLoaderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFuzzy.Service
+{
+    public class AssemblyLoaderService
+    {
+        public string getInfo(string filePath)
+        {
+            string ans = "";
+            try
+            {
+                ans = Assembly.LoadFrom(filePath).FullName;
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine($"Failed according to -> {exp.Message}\n");
+            }
+            return ans;
+        }
+    }
+}
