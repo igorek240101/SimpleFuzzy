a9e9f8e961926ffffc6665b957daccb63963b740
Timofey Elin <140144857+timofeyelin@users.noreply.github.com>
2024-07-11 10:15:45 +0300

    SF-11

---
+++ SimpleFuzzy/SimpleFuzzy.Abstract/IMembershipFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFuzzy.Abstract
+{
+    public interface IMembershipFunction
+    {
+        double membershipFunc(object elem);
+        Type InputType { get; }
+    }
+}
