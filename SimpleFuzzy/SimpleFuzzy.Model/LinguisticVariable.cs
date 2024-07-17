79261de4559a06cbd39bc95d6930409c3b63a906
MaxMen5 <161643770+MaxMen5@users.noreply.github.com>
2024-07-17 14:58:46 +0300

    SF-13 Исправление 4

--- SimpleFuzzy/SimpleFuzzy.Model/LinguisticVariable.cs
+++ SimpleFuzzy/SimpleFuzzy.Model/LinguisticVariable.cs
@@ -6,29 +6,39 @@ namespace SimpleFuzzy.Model;
 
 public class LinguisticVariable
 {
-    public string? Name { get; set; } // Имя лингвистической переменной
-    public IObjectSet BaseSet { get; set; } // Базовое множество
-    public List<IMembershipFunction> Func = new List<IMembershipFunction>(); // Список термов
-    public readonly bool IsRedact; // Возможность редактирования
-    public LinguisticVariable(bool Redact) { IsRedact = Redact; }
+    public string name; // Имя лингвистической переменной
+    public string Name 
+    { 
+        get { return name; }
+        set { if (isRedact == true) { name = value; } } 
+    }
+    public IObjectSet baseSet; // Базовое множество
+    public IObjectSet BaseSet
+    {
+        get { return baseSet; }
+        set { if (isRedact == true) { baseSet = value; } }
+    }
+    public List<IMembershipFunction> func = new List<IMembershipFunction>(); // Список термов
+    public readonly bool isRedact; // Возможность редактирования
+    public LinguisticVariable(bool isRedact) { this.isRedact = isRedact; }
     
     public void AddTerm(IMembershipFunction term) 
     {
-        Type Type1 = BaseSet.Extraction().GetType(), Type2 = Func[0].GetType(); // Проверка типов данных
+        Type Type1 = baseSet.Extraction().GetType(), Type2 = func[0].GetType(); // Проверка типов данных
         if (Type1 != Type2) { throw new InvalidOperationException("Выводимый и запрашиваемый тип данных не совпадают"); }
-        else { Func.Add(term); }
+        else { func.Add(term); }
     }
-    public void DeleteTerm(IMembershipFunction term) => Func.Remove(term); 
+    public void DeleteTerm(IMembershipFunction term) => func.Remove(term); 
     public List<double>[] Graphic()  // Создание массива списков для графика
     {
-        List<double>[] list = new List<double>[Func.Count()];
-        for (int i = 0; i < Func.Count(); i++)
+        List<double>[] list = new List<double>[func.Count()];
+        for (int i = 0; i < func.Count(); i++)
         {
-            BaseSet.ToFirst();
-            while (!BaseSet.IsEnd())
+            baseSet.ToFirst();
+            while (!baseSet.IsEnd())
             {
-                list[i].Add(Func[i].MembershipFunction(BaseSet.Extraction()));
-                BaseSet.MoveNext();
+                list[i].Add(func[i].MembershipFunction(baseSet.Extraction()));
+                baseSet.MoveNext();
             }
         }
         return list;
