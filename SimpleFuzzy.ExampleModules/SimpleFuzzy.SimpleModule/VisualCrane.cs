249bdbc415952e081eeb81d09a7dcaae1b0fc430
BrounDoge <119942622+AlexSergy@users.noreply.github.com>
2024-07-23 00:21:08 +0500

    Исправления№1-Frontend-SF-12

--- SimpleFuzzy.ExampleModules/SimpleFuzzy.SimpleModule/VisualCrane.cs
+++ SimpleFuzzy.ExampleModules/SimpleFuzzy.SimpleModule/VisualCrane.cs
@@ -1,16 +1,9 @@
 ﻿using SimpleFuzzy.SimpleModule;
 using SimpleFuzzy.Abstract;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace SimpleFuzzy.View
