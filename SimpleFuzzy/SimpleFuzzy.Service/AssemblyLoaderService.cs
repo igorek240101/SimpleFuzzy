ad4ff3459e2da398986b3cf3fe3f08e6ca18d000
Acrist671 <mishamironovmma@gmail.com>
2024-07-24 17:03:15 +0600

    SF-25

--- SimpleFuzzy/SimpleFuzzy.Service/AssemblyLoaderService.cs
+++ SimpleFuzzy/SimpleFuzzy.Service/AssemblyLoaderService.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleFuzzy.Abstract;
 using System.Reflection;
+using System.Runtime.Loader;
 
 namespace SimpleFuzzy.Service
 {
@@ -18,5 +19,18 @@ namespace SimpleFuzzy.Service
             }
             return ans;
         }
+        public void UnloadAssembly(string? filePath)
+        {
+            if (filePath is string)
+            {
+                var assemblyContext = new AssemblyLoadContext(name: "UnloadAssemplyContext", isCollectible: true);
+                assemblyContext.LoadFromAssemblyPath(filePath);
+                assemblyContext.Unload();
+            }
+            else
+            {
+                throw new InvalidOperationException("Путь к библиотеке не может быть null");
+            }
+        }   
     }
 }
