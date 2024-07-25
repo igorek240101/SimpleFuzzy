edf4170b1253a783e3e6d1710002b0e3d4ea685b
BrounDoge <119942622+AlexSergy@users.noreply.github.com>
2024-07-25 06:49:33 +0500

    Frontend-SF-15

--- SimpleFuzzy/SimpleFuzzy.View/MainWindow.cs
+++ SimpleFuzzy/SimpleFuzzy.View/MainWindow.cs
@@ -1,10 +1,14 @@
+
 namespace SimpleFuzzy.View
 {
     public partial class MainWindow : Form
     {
+        private GenerationMembershipUI generationMembershipUI;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
 }
