4fabf20b18fba4ca0e6808edbc225ec252cc8ab0
BrounDoge <119942622+AlexSergy@users.noreply.github.com>
2024-07-30 10:35:39 +0500

    Перевод на MetroFramework-SF-9

--- SimpleFuzzy/SimpleFuzzy.View/LoaderForm.Designer.cs
+++ SimpleFuzzy/SimpleFuzzy.View/LoaderForm.Designer.cs
@@ -1,13 +1,9 @@
-﻿namespace SimpleFuzzy.View
-{
+﻿
+namespace SimpleFuzzy.View 
+{ 
     partial class LoaderForm
     {
         private System.ComponentModel.IContainer components = null;
-
-        private TextBox filePathTextBox;
-        private Button browseButton;
-        private Button loadButton;
-        private TextBox messageTextBox;
 
         protected override void Dispose(bool disposing)
         {
@@ -22,61 +18,80 @@
 
         private void InitializeComponent()
         {
-            filePathTextBox = new TextBox();
-            browseButton = new Button();
-            loadButton = new Button();
-            messageTextBox = new TextBox();
-            SuspendLayout();
+            this.filePathTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.browseButton = new MetroFramework.Controls.MetroButton();
+            this.loadButton = new MetroFramework.Controls.MetroButton();
+            this.messageTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
             // 
             // filePathTextBox
             // 
-            filePathTextBox.Location = new Point(12, 12);
-            filePathTextBox.Name = "filePathTextBox";
-            filePathTextBox.Size = new Size(399, 27);
-            filePathTextBox.TabIndex = 1;
+            this.filePathTextBox.Location = new System.Drawing.Point(23, 63);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(399, 23);
+            this.filePathTextBox.TabIndex = 0;
+            this.filePathTextBox.BackColor = System.Drawing.Color.White;
+            this.filePathTextBox.ForeColor = System.Drawing.Color.Black;
+            this.filePathTextBox.Font = new System.Drawing.Font("Microsoft Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             // 
             // browseButton
             // 
-            browseButton.Location = new Point(417, 9);
-            browseButton.Name = "browseButton";
-            browseButton.Size = new Size(75, 30);
-            browseButton.TabIndex = 2;
-            browseButton.Text = "Обзор";
-            browseButton.UseVisualStyleBackColor = true;
-            browseButton.Click += browseButton_Click;
+            this.browseButton.Location = new System.Drawing.Point(428, 63);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(100, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Обзор";
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            this.browseButton.BackColor = System.Drawing.ColorTranslator.FromHtml("#0B61A4");
+            this.browseButton.ForeColor = System.Drawing.Color.White;
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             // 
             // loadButton
             // 
-            loadButton.Location = new Point(12, 45);
-            loadButton.Name = "loadButton";
-            loadButton.Size = new Size(200, 30);
-            loadButton.TabIndex = 3;
-            loadButton.Text = "Загрузить модуль";
-            loadButton.UseVisualStyleBackColor = true;
-            loadButton.Click += loadButton_Click;
+            this.loadButton.Location = new System.Drawing.Point(23, 92);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(200, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Загрузить модуль";
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            this.loadButton.BackColor = System.Drawing.ColorTranslator.FromHtml("#00AF64");
+            this.loadButton.ForeColor = System.Drawing.Color.White;
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             // 
             // messageTextBox
             // 
-            messageTextBox.Location = new Point(12, 80);
-            messageTextBox.Multiline = true;
-            messageTextBox.Name = "messageTextBox";
-            messageTextBox.ReadOnly = true;
-            messageTextBox.ScrollBars = ScrollBars.Vertical;
-            messageTextBox.Size = new Size(480, 100);
-            messageTextBox.TabIndex = 4;
+            this.messageTextBox.Location = new System.Drawing.Point(23, 121);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.ReadOnly = true;
+            this.messageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messageTextBox.Size = new System.Drawing.Size(505, 100);
+            this.messageTextBox.TabIndex = 3;
+            this.messageTextBox.BackColor = System.Drawing.Color.White;
+            this.messageTextBox.ForeColor = System.Drawing.Color.Black;
+            this.messageTextBox.Font = new System.Drawing.Font("Microsoft Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             // 
-            // LoaderForm
+            // Form1
             // 
-            Controls.Add(filePathTextBox);
-            Controls.Add(browseButton);
-            Controls.Add(loadButton);
-            Controls.Add(messageTextBox);
-            Name = "LoaderForm";
-            Size = new Size(504, 187);
-            ResumeLayout(false);
-            PerformLayout();
+            this.ClientSize = new System.Drawing.Size(551, 244);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Name = "Form1";
+            this.Text = "Загрузчик модулей";
+            this.BackColor = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTextBox filePathTextBox;
+        private MetroFramework.Controls.MetroButton browseButton;
+        private MetroFramework.Controls.MetroButton loadButton;
+        private MetroFramework.Controls.MetroTextBox messageTextBox;
     }
 }
